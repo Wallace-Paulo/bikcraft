4a383ee8e6cc22cc11c1d19f0005cb2293595848
Wallace-Paulo <wpdesigneer@gmail.com>
2022-11-10 16:04:51 -0300

    commit TESTANDO JS

---
+++ javascript/script.js
@@ -0,0 +1,12 @@
+const nome = "O Senhor dos Anéis"
+const ano = 1954
+const autor = "Wallace P. T. de Oliveira"
+
+
+const livro = {
+     nome:"O Senhor dos Anéis",
+     ano:1954,
+     autor:"Wallace P. T. de Oliveira", 
+}
+
+console.log (livro)
